76eaf237c7d47a3fd0e9f3e08f7b0bb585559148
JB <jake.baker0630@gmail.com>
2024-05-11 07:43:46 -0700

    SDL boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
+#include <SDL.h>
 #include <iostream>
-#include "App.h"
 
-int main(int /*argc*/, char** /*argv*/)
-{
-    std::cout << "Hello, world!" << std::endl;
+#include "App.hpp"
+
+int main(int /*argc*/, char** /*argv[]*/) {
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Could not initialize SDL " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("0xDEADBEEF",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          640, 480,
+                                          SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event event;
+    bool running = true;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
